5d27cd5a17ee2423d6ec9b50227f753f1ec3f433
D3Portillo <d3portillo@gmail.com>
2022-08-21 06:11:47 +0200

    enh(Sketch.ts): Proxy Sketch#setup to apply #prepareSetup fn

--- src/Sketch.ts
+++ src/Sketch.ts
@@ -1,7 +1,20 @@
 import P5 from "p5"
 
 export default class Sketch extends P5 {
-  public static start(node: HTMLElement) {
-    new this((p) => p, node)
+  canvasSize = 480 // Default size for any created canvas
+
+  public static start() {
+    new this((p) => p, document.querySelector("#renderer")!)
+  }
+
+  private prepareSetup(setup: Function) {
+    this.createCanvas(this.canvasSize, this.canvasSize)
+    setup.call(this)
+  }
+
+  constructor(sketch: (p5: P5) => any, node: HTMLElement) {
+    super(sketch, node)
+    const __setup = this.setup // Proxy setup method
+    this.setup = () => this.prepareSetup(__setup)
   }
 }
