2e305b37a820b52a9104fc30531ae75d966e92db
D3Portillo <d3portillo@gmail.com>
2022-08-21 04:50:10 +0200

    chores: refactor code to match .prettierrc

--- src/Sketch.ts
+++ src/Sketch.ts
@@ -1,7 +1,7 @@
-import P5 from "p5";
+import P5 from "p5"
 
 export default class Sketch extends P5 {
   public static start(node: HTMLElement) {
-    new this((p) => p, node);
+    new this((p) => p, node)
   }
 }
--- src/sketches/Playground.ts
+++ src/sketches/Playground.ts
@@ -1,34 +1,34 @@
-import Sketch from "../Sketch";
+import Sketch from "../Sketch"
 
 class Playground extends Sketch {
   setup() {
-    this.createCanvas(400, 400);
-    this.background(255, 255, 255);
+    this.createCanvas(400, 400)
+    this.background(255, 255, 255)
 
     // translucent stroke using alpha value
-    this.stroke(0, 0, 0, 15);
+    this.stroke(0, 0, 0, 15)
   }
 
   draw() {
     // draw two random chords each frame
-    this.randomChord();
-    this.randomChord();
+    this.randomChord()
+    this.randomChord()
   }
 
   randomChord() {
     // find a random point on a circle
-    let angle1 = this.random(0, 2 * this.PI);
-    let xpos1 = 200 + 200 * this.cos(angle1);
-    let ypos1 = 200 + 200 * this.sin(angle1);
+    let angle1 = this.random(0, 2 * this.PI)
+    let xpos1 = 200 + 200 * this.cos(angle1)
+    let ypos1 = 200 + 200 * this.sin(angle1)
 
     // find another random point on the circle
-    let angle2 = this.random(0, 2 * this.PI);
-    let xpos2 = 200 + 200 * this.cos(angle2);
-    let ypos2 = 200 + 200 * this.sin(angle2);
+    let angle2 = this.random(0, 2 * this.PI)
+    let xpos2 = 200 + 200 * this.cos(angle2)
+    let ypos2 = 200 + 200 * this.sin(angle2)
 
     // draw a line between them
-    this.line(xpos1, ypos1, xpos2, ypos2);
+    this.line(xpos1, ypos1, xpos2, ypos2)
   }
 }
 
-export default Playground;
+export default Playground
