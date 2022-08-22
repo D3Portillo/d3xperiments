1b4dfef6fe9aabfa6d1ce1c5ec6c493f6fd4632b
D3Portillo <d3portillo@gmail.com>
2022-08-22 03:37:06 +0200

    enh(Noise.ts): cuter and smoother version for this sketch :3

--- src/sketches/Noise.ts
+++ src/sketches/Noise.ts
@@ -1,27 +1,41 @@
 import Sketch from "../Sketch"
 
 export default class Noise extends Sketch {
-  xOffset = 0.0
-  yOffset = 0.0
+  xOffset = 0
+  yOffset = 0
+  yDestination = 0
+  lastYDestination = 0
 
   setup() {
+    this.yDestination = this.canvasSize * 0.75
     this.noStroke()
   }
 
   draw() {
-    const { xOffset, yOffset, canvasSize: size } = this
-    const speed = 0.009
+    const {
+      xOffset,
+      yOffset,
+      canvasSize: size,
+      yDestination,
+      lastYDestination,
+    } = this
+    const SPEED = 0.009
     // Create an alpha blended background
-    this.fill(248, 33)
+    this.fill(250, 25)
     this.rect(0, 0, size, size)
 
-    // Get a noise value based on xoff and scale
+    // Get a noise value based on xoff and canvas size
     const xNoise = this.noise(xOffset) * size
-    const yNoise = this.noise(yOffset) * size
+    const currY = this.lerp(lastYDestination, yDestination, this.yOffset)
+    if (yOffset >= 1) {
+      this.yOffset = 0
+      this.lastYDestination = yDestination
+      this.yDestination = this.random(0, size)
+    }
     const ballSize = 42 * 2
     this.fill(42)
-    this.ellipse(xNoise, yNoise, ballSize, ballSize)
-    this.xOffset += speed
-    this.yOffset += speed
+    this.ellipse(xNoise, currY, ballSize, ballSize)
+    this.xOffset += SPEED
+    this.yOffset += SPEED
   }
 }
