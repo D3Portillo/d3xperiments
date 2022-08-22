56e7462fd840b75fa7b5f1dc6e03c1af47f5fe6f
D3Portillo <d3portillo@gmail.com>
2022-08-22 03:34:20 +0200

    enh(sketches/list.ts): export getSketchName,getSketchURL utils

--- src/sketches/list.ts
+++ src/sketches/list.ts
@@ -1,4 +1,41 @@
 import Playground from "./Playground"
 import Noise from "./Noise"
+import SketchClass from "../Sketch"
 
-export default [Playground, Noise]
+type Sketch = typeof SketchClass
+const list: Array<Sketch> = []
+const sketchNames: Array<string> = []
+const sketchURLs: Array<string> = []
+
+function setSketchList(
+  ...args: Array<{ sketch: Sketch; name: string; url: string }>
+) {
+  args.map(({ sketch, name, url }) => {
+    list.push(sketch)
+    sketchNames.push(name)
+    sketchURLs.push(url)
+  })
+}
+
+function __getSketchURL(file: string) {
+  return `https://github.com/D3Portillo/d3xperiments/blob/master/src/sketches/${file}`
+}
+
+setSketchList(
+  {
+    sketch: Playground,
+    name: "Genesis",
+    url: __getSketchURL("Playground.ts"),
+  },
+  {
+    sketch: Noise,
+    name: "Noise",
+    url: __getSketchURL("Noise.ts"),
+  }
+)
+
+export const size = list.length
+export const lastIndex = size - 1
+export const getSketchName = (idx: number) => sketchNames[idx]
+export const getSketchURL = (idx: number) => sketchURLs[idx]
+export default list
