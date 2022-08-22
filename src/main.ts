a1365aabc5793f142ededcba716b4eb5b09aa65b
D3Portillo <d3portillo@gmail.com>
2022-08-22 03:35:46 +0200

    fix(main.ts): inject sketch.index,instance props

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import "./style.css"
+import { html } from "lighterhtml"
+
+import App from "./components/App"
 import SketchList from "./sketches/list"
 
-const sketchIdx = parseInt(location.hash.replace(/#|\//g, "") || "0")
+/**
+ * If no 404 file present Cloudfare considers the deploy as an SPA.
+ * We can then extract path to match sketch item to render
+ * @see https://developers.cloudflare.com/pages/platform/serving-pages/#single-page-app-spa-rendering
+ */
+const sketchIdx = parseInt(location.pathname.replace("/", "") || "0")
 const toRenderSketch = SketchList[sketchIdx]
+window.sketch = { index: sketchIdx, instance: toRenderSketch }
 
 if (toRenderSketch) {
+  window.app.appendChild(html.node`${App()}`)
   toRenderSketch.start()
 } else {
   // TODO: render 404
